b0ffc267777b0350af068cc1aa36ad3d153387de
Sachin Kumar Verma <skvbca32@gmail.com>
2026-02-28 10:53:35 +0530

    Your message

--- src/main/java/com/sachin/webLoan/controller/HomeController.java
+++ src/main/java/com/sachin/webLoan/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.sachin.webLoan.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
